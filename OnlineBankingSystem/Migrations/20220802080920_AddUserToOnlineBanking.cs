771c6dd1a11d279c4cd63ef3c1699da71d7d442b
CHIRU3223 <madireddy.csk0123@gmail.com>
2022-08-02 15:35:16 +0530

    User Model + Sql Server integration

---
+++ OnlineBankingSystem/Migrations/20220802080920_AddUserToOnlineBanking.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineBankingSystem.Migrations
+{
+    public partial class AddUserToOnlineBanking : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Username = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNo = table.Column<int>(type: "int", nullable: false),
+                    SSN = table.Column<int>(type: "int", nullable: false),
+                    DoB = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    isAdmin = table.Column<bool>(type: "bit", nullable: false),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NoOfAccounts = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Username);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ OnlineBankingSystem/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineBankingSystem.Models
+{
+    public class User
+    {
+        [Key]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int PhoneNo { get; set; }
+        [Required]
+        public int SSN { get; set; }
+
+        [Required]
+        public DateTime DoB { get; set; }
+
+        [Required]
+        public DateTime UserCreated { get; set; }
+        [Required]
+        public Boolean isAdmin { get; set; }
+        [Required]
+        public string email { get; set; }
+
+        public int NoOfAccounts { get; set; }
+    }
+}
